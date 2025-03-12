4f03dbe88095e029dffc554a1c739e8b1e8ebde6
TLdgh <teng.li2@mail.mcgill.ca>
2025-03-12 02:04:57 -0400

    minor adj to app.R.

--- LLMexp/app.R
+++ LLMexp/app.R
@@ -4,7 +4,7 @@ library(shiny)
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Experimental Outcome"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
